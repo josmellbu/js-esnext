7ff0ec57c53b7f8e9bfd3835cc96dfa27d7b029f
ciber-pasito <ciber@pasito.com>
2023-08-18 08:44:47 -0500

    fisrt commit

---
+++ 01-arrays/array-to-methods.js
@@ -0,0 +1,7 @@
+const heroes = ['Batman', 'Superman', 'Flash','Aquaman'];
+// const heroesCopy = heroes;
+
+const sortedHeroes = heroes.toSorted();
+
+console.table(heroes);
+console.table(sortedHeroes);
---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+// import './01-arrays/array-with'
+// import './01-arrays/array-structured-clone'
+import './01-arrays/array-to-methods'
+
+document.querySelector('#app').innerHTML = `
+    <h1>Hello Vite!</h1>
+`
